f9a492d67bbabaf7999bc9763a769bc099f2dd1a
Ivan Gulakov <gulakov@protonmail.com>
2024-02-15 10:09:10 +0000

    Add sound generation
    Used bark as an initial sound generation service. Works bad on
    my old thinkpad though. Might see some improvements on macbook m1pro
    due to a new AI-chip on it.
    
    Also added a requirements.txt file, but that should be changed to
    py project anyway.

--- execute.py
+++ execute.py
@@ -1,4 +1,43 @@
 import os
+from pydub import AudioSegment
+from pydub.generators import Sine
+import pygame
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+
+'''
+TODO: 
+* Update voice generation to this: https://github.com/coqui-ai/TTS
+* Fetch an image
+* Fetch amount of meanings (how? Cambridge dictionary?)
+* Fetch voices
+* Settings
+'''
+
+preload_models()
+
+pygame.mixer.init()
+
+def synthesize_and_play_sound(word):
+
+    audio_array = generate_audio(word)
+
+    # save audio to disk
+    write_wav(f"{word}.wav", SAMPLE_RATE, audio_array)
+    # synthesized_audio = gTTS(text=word, lang='en', tld="us", slow=0.8)
+
+    # # Save the audio to a file (optional)
+    # synthesized_audio.save(f"{word}.mp3")
+
+    # Load the MP3 file
+    pygame.mixer.music.load(f"{word}.wav")
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Optional: Wait for the song to finish
+    while pygame.mixer.music.get_busy():
+        pygame.time.wait(100)
 
 # Function to load the history of shown words
 def load_history(filename):
@@ -18,11 +57,11 @@ def save_history(history, filename):
 # Function to prompt user if they know a word and handle their response
 def prompt_user(word):
     while True:
-        response = input(f"Next word:      '{word}'?").lower()
-        if response in ["y", "n"]:
-            return response == "no"
+        response = input(f"\n'{word}'").lower()
+        if response in ["y", "n", ""]:
+            return response == "n"
         else:
-            print("Please respond with 'y' or 'n'.")
+            print("Please respond with 'y' or 'n'. Hit enter in case of yes")
 
 # Function to filter out known words and update the file
 def filter_file(file_path, history):
@@ -32,7 +71,7 @@ def filter_file(file_path, history):
             if line not in history:
                 new_lines.append(line)
     with open(file_path, "w") as f:
-        f.write("\n".join(new_lines))
+        f.write("".join(new_lines))
 
 def main():
     known_history = load_history("known.txt")
@@ -48,14 +87,14 @@ def main():
             word = line.strip()
             if word in known_history or word in unknown_history:
                 continue
+            synthesize_and_play_sound(word)
             if prompt_user(word):
                 unknown_history.add(word)
-                save_history(unknown_history, "known.txt")
+                save_history(unknown_history, "unknown.txt")
             else:
                 known_history.add(word)
                 filter_file(file_path, known_history)
-                save_history(known_history, "unknown.txt")
-                break
+                save_history(known_history, "known.txt")
 
     print("Done.")
 
